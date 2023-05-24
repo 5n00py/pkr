4bf77afb63d50fa43c2819a8d6847dd27c04e62c
David Schmid <david.schmid@mailbox.org>
2023-05-24 09:07:54 +0200

    Add num_value() Method to Rank
    - Add num_value() to Rank in rank.rs
    - Method allows retrieving numerical value of a card's rank mapping each
      rank to its corresonding integer value

--- src/card/rank.rs
+++ src/card/rank.rs
@@ -100,6 +100,29 @@ impl Rank {
             Rank::Ace => "A",
         }
     }
+
+    /// Returns the numerical value of a card's rank.
+    ///
+    /// # Returns
+    ///
+    /// An integer corresponding to the numerical value of the card's rank.
+    pub fn num_value(&self) -> u32 {
+        match self {
+            Rank::Two => 2,
+            Rank::Three => 3,
+            Rank::Four => 4,
+            Rank::Five => 5,
+            Rank::Six => 6,
+            Rank::Seven => 7,
+            Rank::Eight => 8,
+            Rank::Nine => 9,
+            Rank::Ten => 10,
+            Rank::Jack => 11,
+            Rank::Queen => 12,
+            Rank::King => 13,
+            Rank::Ace => 14,
+        }
+    }
 }
 
 #[cfg(test)]
